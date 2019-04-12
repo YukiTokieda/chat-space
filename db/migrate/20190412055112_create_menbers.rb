d65ea553c3095fb2f0d0eac9bc5b086654e86483
yuki tokieda <tokki7127@icloud.com>
2019-04-12 18:16:02 +0900

    Fix migrate/menber
    referencesに_idはいらないらしい。

--- db/migrate/20190412055112_create_menbers.rb
+++ db/migrate/20190412055112_create_menbers.rb
@@ -1,8 +1,8 @@
 class CreateMenbers < ActiveRecord::Migration[5.0]
   def change
     create_table :menbers do |t|
-      t.references :uers_id,null: false,unique: true
-      t.references :groups_id,null: false,unique: true
+      t.references :uers,null: false,unique: true
+      t.references :groups,null: false,unique: true
       t.timestamps
     end
   end
