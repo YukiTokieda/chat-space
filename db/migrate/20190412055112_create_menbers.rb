f8d9cfd43a73f5bf8d1f3b175610e400d5c21db1
yuki tokieda <tokki7127@icloud.com>
2019-04-12 15:53:36 +0900

    K

--- db/migrate/20190412055112_create_menbers.rb
+++ db/migrate/20190412055112_create_menbers.rb
@@ -1,8 +1,8 @@
 class CreateMenbers < ActiveRecord::Migration[5.0]
   def change
     create_table :menbers do |t|
-      t.integer :uers_id
-      t.integer :groups_id
+      t.integer :uers_id,null: false,unique: true
+      t.integer :groups_id,null: false,unique: true
       t.timestamps
     end
   end
