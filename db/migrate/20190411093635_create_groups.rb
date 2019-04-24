8514ed2a01f5f3c1d6765bb0de6f98d938488e43
YukiTokieda <49357782+YukiTokieda@users.noreply.github.com>
2019-04-24 09:59:30 +0900

    Revert "Message function"

--- db/migrate/20190411093635_create_groups.rb
+++ db/migrate/20190411093635_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :name, null: false, unique: true
+      t.string :name,null: false
+      t.integer :user_id,null: false
       t.timestamps
     end
   end
