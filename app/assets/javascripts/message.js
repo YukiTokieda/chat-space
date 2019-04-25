162b92205a9c55cd49f72db9d46372e6b1ed9791
yuki tokieda <tokki7127@icloud.com>
2019-04-25 14:31:27 +0900

    console.log(this)
    削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
+
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
