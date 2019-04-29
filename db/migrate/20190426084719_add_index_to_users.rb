6a7b7c2af648a2952a8c2f52c02da6d2fb9c904a
yuki tokieda <tokki7127@icloud.com>
2019-04-29 17:20:41 +0900

    Fixed migrate

--- db/migrate/20190426084719_add_index_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_index :users,  :name
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190426084719) do
+ActiveRecord::Schema.define(version: 20190419044406) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
