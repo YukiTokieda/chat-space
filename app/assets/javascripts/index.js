c16454765f21f7708530df2cde7486fb73c1ff5c
yuki tokieda <tokki7127@icloud.com>
2019-04-29 16:49:22 +0900

    After Incremental search

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,23 +1,24 @@
-$(function() {
-  var search_list = $("#user-search-result");
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    var search_list = $("#user-search-result");
+
   function appendUser(user) {
     var html =
     `<div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${ user.name }</p>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
      </div>`
       search_list.append(html);
    }
 
-   function appendErrMsgToHTML(msg) {
-    var html =
-    `
-    <div class='listview__element--right-icon'>${ msg }</div>
-    `
-        search_list.append(html);
-   }
+  function appendErrMsgToHTML(user){
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user}</p>
+                </div>`
+    search_list.append(html);
+  }
 
-  $(".chat-group-form__input").on("keyup", function() {
+  $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
   $.ajax({
@@ -32,7 +33,6 @@ $(function() {
     if (users.length !== 0) {
       users.forEach(function(user){
         appendUser(user);
-        console.log(users)
       });
     }
     else {
@@ -43,6 +43,30 @@ $(function() {
   .fail(function() {
     alert('ユーザー検索に失敗しました');
   })
+  });
+
+  function addUser(user_id, user_name) {
+    console.log(user_id, user_name)
+   var html =
+    `<div class='chat-group-user clearfix' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+      <p class='chat-group-user__name'>${user_name}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+  $('#chat-group-users').append(html);
+}
+
+  $('#user-search-result').on("click",'.chat-group-user__btn--add',function() {
+   var user_id = $(this).data('user-id');
+   var user_name = $(this).data('user-name');
+    addUser(user_id, user_name);
+    $(this).parent().remove();
+
+  });
+
+  $('#chat-group-users').on('click','.chat-group-user__btn--remove',function(){
+    $(this).parent().remove();
+  });
 
  });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?) AND id !=(?)', "%#{params[:keyword]}%", current_user.id)
     respond_to do |format|
       format.html
       format.json
