a12f3f14379b1f7234550ec2d4c9b2b0fd3280e4
yuki tokieda <tokki7127@icloud.com>
2019-05-01 15:31:03 +0900

    提出

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,17 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_group
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
     respond_to do |format|
       format.html
       format.json{ @new_messages = @messages.where('id > ?', params[:id]) }
+    end
   end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
 end
